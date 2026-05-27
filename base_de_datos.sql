82e9ef8363b2d58564db0156344a92c658d3956d
Marely Amaya <6050952@incb.edu.sv>
2026-05-27 08:16:31 -0600

    Añadidas consultas estadísticas

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -59,3 +59,28 @@ FROM productos p
 INNER JOIN categorias c
 ON p.categoria_id = c.id
 WHERE c.nombre_categoria = 'Accesorios';
+
+
+-- ====================================================================
+-- CONSULTAS DE ESTADÍSTICAS Y MÉTRICAS PARA EL DASHBOARD (Guía 12)
+-- ====================================================================
+
+-- Tarjeta 1
+SELECT COUNT(id) AS total_productos_catalogo
+FROM productos;
+
+-- Tarjeta 2
+SELECT SUM(precio * stock) AS valor_monetario_inventario
+FROM productos;
+
+-- Tarjeta 3
+SELECT MAX(precio) AS producto_mas_caro
+FROM productos;
+
+-- Tarjeta 4
+SELECT c.nombre_categoria,
+SUM(p.stock) AS existencias_totales
+FROM productos p
+INNER JOIN categorias c
+ON p.categoria_id = c.id
+GROUP BY c.nombre_categoria;
