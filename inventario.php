6322934298bd817647071b309566e5115a6c473f
Marely Amaya <6050952@incb.edu.sv>
2026-08-12 08:13:30 -0600

    Cierre de Módulo: Agregado buscador en tiempo real usando LIKE y GET

--- inventario.php
+++ inventario.php
@@ -103,11 +103,17 @@ $claseStock = ($fila['stock'] < 10) ? 'stock-bajo' : '';
 <td class="<?php echo $claseStock; ?>"> <?php echo $fila['stock']; ?> unds. </td>
 <td> $<?php echo number_format($fila['precio'], 2); ?> </td>
 <td>
+
+<a href="editar_producto.php?id=<?php echo $fila['id']; ?>" class="btn-editar">
+✏️ Editar
+</a>
+
 <a href="eliminar_producto.php?id=<?php echo $fila['id']; ?>"
 class="btn-eliminar"
-onclick="return confirm('¿Estás absolutamente seguro de eliminar el producto: <?php echo $fila['nombre_producto']; ?>?');">
+onclick="return confirm('¿Seguro?');">
 🗑️ Eliminar
 </a>
+
 </td>
 </tr>
 <?php
