3f503ebe8808ba68d29d4ae1366fbd7844a2617a
Marely Amaya <6050952@incb.edu.sv>
2026-08-13 07:44:08 -0600

    Inicio del módulo de compras: Agregada tabla y vista de proveedores

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -23,6 +23,20 @@ precio DECIMAL(10, 2) NOT NULL,
 FOREIGN KEY (categoria_id) REFERENCES categorias(id)
 );
 
+CREATE TABLE proveedores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_empresa VARCHAR(100) NOT NULL,
+    contacto VARCHAR(100),
+    telefono VARCHAR(20),
+    direccion TEXT
+);
+
+INSERT INTO proveedores 
+(nombre_empresa, contacto, telefono, direccion) 
+VALUES
+('Tech Data El Salvador', 'Juan Pérez', '2255-8899', 'San Salvador, Col. Escalón'),
+('Distribuidora de Papel', 'María Gómez', '2666-4433', 'San Miguel, Centro');
+
 -- 4. Inserción de los catálogos base (deben insertarse primero)
 INSERT INTO categorias (nombre_categoria) VALUES
 ('Computadoras'),
